eeb99d9615a44d7390285688a40f265ca2bd47ea
pivograd <e.pivograd@yandex.ru>
2023-12-21 01:32:11 +0300

    изменил скорость бегущей строки

--- views.py
+++ views.py
@@ -37,13 +37,15 @@ def generate_running_line(request, text):
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     video_writer = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
     num_frames = 90
+    total_length_char = len(text) * width / 3
+    total_letter_spacing = (len(text) - 1) * width / 7.5
+    total_length_string = total_length_char + total_letter_spacing
+    delta_x_1 = (total_length_string + width + width / 3) / num_frames  # Изменение x, чтобы бегущая строка заканчивалась белым экраном
+    delta_x_2 = (total_length_string + width) / num_frames  # Изменение x, чтобы бегущая строка заканчивалась последним символом
     for frame in range(num_frames):
         draw.rectangle((0, 0, width, height), fill=(255, 255, 255))
         draw.text((x_position, 0), text, font=font, fill=(0, 0, 0))
-        if len(text) > 7:
-            x_position -= len(text) - (len(text) / 3) + 1
-        else:
-            x_position -= 5
+        x_position -= delta_x_1
         frame_array = np.array(image)
         # Инвертирую цвета (OpenCV использует BGR, а не RGB)
         frame_array = cv2.cvtColor(frame_array, cv2.COLOR_RGB2BGR)
